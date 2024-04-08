b2c57bdda03370563db9a3a57e7a1068707cfd18
Jackson Miller <milljac8@oregonstate.edu>
2024-04-08 16:24:18 -0700

    adding figlet and code for printing val?

--- asciiArt.js
+++ asciiArt.js
@@ -3,3 +3,13 @@ import { argv } from 'node:process';
 
 console.log(`${val[2]}`);
 
+var figlet = require("figlet");
+
+figlet(`${val[2]}`, function (err, data){
+    if (err) {
+        console.log("Something went wrong...");
+        console.dir(err);
+        return;
+    }
+    console.log(data);
+});
